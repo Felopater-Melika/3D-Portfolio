7b2052984f9a9603a304da18a0ca35fd864b38b7
Philopater Phoenix <Philopater.phoenix@gmail.com>
2023-03-20 20:39:47 -0500

    commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 import { styles } from "@/styles";
 import { ComputersCanvas } from "./canvas";
+import Link from "next/link";
 const Hero = () => {
   return (
-    <main className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto">
       <div
         className={`${styles.paddingX} absolute  top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -17,9 +19,29 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Felopater</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
         </div>
       </div>
-    </main>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <Link href={"/#about"}>
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </Link>
+      </div>
+    </section>
   );
 };
 
